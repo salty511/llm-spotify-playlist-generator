7a796b0f63eb530534870843868ba93c21f53c62
salty511 <isaac.w.salt@gmail.com>
2025-10-07 17:27:58 +0100

    install sass and setup custom variables

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+import './index.scss'
 import App from './App.tsx'
 
 // Enable dark theme globally
